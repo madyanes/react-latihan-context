f7c18d595dfee74a13517509fef3632aa90c9955
madyanes <madyanes.dev@gmail.com>
2023-08-07 07:22:02 +0700

    switch language for content

--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import LocaleContext from '../contexts/LocaleContext';
 import Posts from '../components/Posts';
 
 function Tutorial() {
+  const { locale } = React.useContext(LocaleContext)
+
   return (
     <>
       <h1>Tutorial</h1>
-      <Posts locale="id" />
+      <Posts locale={locale} />
     </>
   );
 }
